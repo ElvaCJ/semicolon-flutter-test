265b20e499b8f0a409891148d6d21ad036d39d82
Chanjoo Park <cjangel0503@gmail.com>
2024-07-21 13:49:47 +0900

    7주차 실습 문제

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:provider/provider.dart';
-import './ex1_0714.dart';
-import './ex2_0714.dart';
+import './ex1_0721.dart';
+import './ex2_0721.dart';
 
 void main() {
-  runApp(ChangeNotifierProvider(
-    create: (BuildContext context) {
-      return Count();
-    },
-    child: const MyApp(),
-  ));
+  runApp(
+    MultiProvider(
+      providers: const [
+        // 변경된 내용을 반영할 수 있도록 각 위젯의 Provider를 등록해야겠죠?
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -20,15 +23,43 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: const HomePage(),
       routes: {
-        '/exam1': (context) => const Ex1(),
-        '/exam2': (context) => const ShoppingMall(),
+        '/exam1': (context) => SelfIntroduce(),
+        '/exam2': (context) => const TodoList(),
       },
     );
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final TextEditingController _controllerText = TextEditingController();
+  bool _isButton1Enabled = false;
+  bool _isButton2Enabled = false;
+
+  void _checkInput() {
+    setState(() {
+      _isButton1Enabled = _controllerText.text == "semicolon";
+      _isButton2Enabled = _controllerText.text == "semicolon";
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _controllerText.addListener(_checkInput);
+  }
+
+  @override
+  void dispose() {
+    _controllerText.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -40,20 +71,42 @@ class HomePage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            TextField(
+              controller: _controllerText,
+              decoration: const InputDecoration(
+                prefixIcon: Icon(Icons.arrow_forward_outlined),
+                hintText: 'semicolon 이라고 입력해보세요',
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                  borderSide: BorderSide(width: 1, color: Colors.grey),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                  borderSide: BorderSide(width: 2, color: Colors.deepPurple),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
             ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/exam1');
-              },
-              child: const Text('실습 1'),
+              onPressed: _isButton1Enabled
+                  ? () {
+                      Navigator.pushNamed(context, '/exam1');
+                    }
+                  : null,
+              child: const Text('Exam1 이동'),
             ),
             const SizedBox(
               height: 10,
             ),
             ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/exam2');
-              },
-              child: const Text('실습 2'),
+              onPressed: _isButton2Enabled
+                  ? () {
+                      Navigator.pushNamed(context, '/exam2');
+                    }
+                  : null,
+              child: const Text('Exam2 이동'),
             ),
             const SizedBox(
               height: 20,
